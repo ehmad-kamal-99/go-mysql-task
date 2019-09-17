750c7e3415b4af6c86d741cc6f824758dc1f49bc
Ahmed Kamal <ehmad.kamal.99@gmail.com>
2019-09-17 23:28:46 +0500

    Code restructured and improved

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Go MySQL Task
 
 A small Golang program that reads data from a csv file and saves it to MySql database.
+
+Instructions:
+1- Install Docker
+2- Install MySQL
+3- git pull
+4- docker-compose up in project directory
+5- Start MySQL Workbench. Start new connection named 'mysql-dev' on port 'localhost:3308' with    default schema 'testdb'. Enter password 'password'.
+6- Start CMD or PowerShell and move to project directory. run command 'docker build -t my-go-app .'
+7- Run application on host machine i.e 'go run main.go'
--- main.go
+++ main.go
@@ -1,17 +1,104 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	"encoding/csv"
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// User structure that stores values retrieved from csv
+type User struct {
+	FirstName  string `json:"firstname"`
+	LastName   string `json:"lastname"`
+	Age        int    `json:"age"`
+	BloodGroup string `json:"bloodgroup"`
+}
+
 func main() {
-	fmt.Println("Docker Practise")
+	FindCsv()
+}
+
+// FindCsv file in a directory
+func FindCsv() {
+	var root string
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("# To exit the program type 'exit'")
+	fmt.Println("Enter the file path to search for csv:")
+
+	dir, _ := reader.ReadString('\n')
+
+	root = strings.TrimRight(dir, "\r\n")
+
+	if root == "exit" {
+		os.Exit(1)
+	} else {
+		files, err := FilePathWalkDir(root)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, file := range files {
+			if filepath.Ext(file) == ".csv" {
+				fmt.Println("File found at: " + filepath.Base(file))
+				ReadCsvFile(filepath.Clean(file))
+			} else if filepath.Ext(file) != ".csv" {
+
+			} else {
+				fmt.Println("No file found.")
+				reader := bufio.NewReader(os.Stdin)
+				fmt.Println("Try again? [y/n]")
+
+				answer, _ := reader.ReadString('\n')
+				ans := strings.TrimRight(answer, "\r\n")
+
+				if ans == "y" || ans == "Y" {
+					FindCsv()
+				} else if ans == "n" || ans == "N" {
+					os.Exit(1)
+				}
+			}
+		}
+	}
+}
+
+// ReadCsvFile function that reads csv files
+func ReadCsvFile(file string) {
+
+	f, _ := os.Open(file)
+	r := csv.NewReader(f)
+
+	for {
+		record, err := r.Read()
+		// Stop at EOF.
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			panic(err)
+		}
+
+		user := new(User)
+		user.FirstName = record[0]
+		user.LastName = record[1]
+		user.Age, _ = strconv.Atoi(record[2])
+		user.BloodGroup = record[3]
+
+		InsertToDb(user)
+	}
+}
+
+// InsertToDb function
+func InsertToDb(user *User) {
 
 	db, err := sql.Open("mysql", "root:password@tcp(localhost:3308)/testdb")
 
@@ -21,30 +108,27 @@ func main() {
 
 	defer db.Close()
 
-	fmt.Println("Connection Successfull!")
+	sqlstatement := `
+	INSERT INTO user (firstname, lastname, age, bloodgroup)
+	VALUES (?, ?, ?, ?)
+	`
 
-	f, _ := os.Open("user_data.csv")
+	_, err = db.Exec(sqlstatement, user.FirstName, user.LastName, user.Age, user.BloodGroup)
 
-	r := csv.NewReader(f)
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			panic(err)
-		}
-
-		sqlstatement := `
-		INSERT INTO user (firstname, lastname, age, bloodgroup)
-		VALUES (?, ?, ?, ?)
-		`
-
-		_, err = db.Exec(sqlstatement, record[0], record[1], record[2], record[3])
-
-		if err != nil {
-			panic(err.Error())
-		}
+	if err != nil {
+		panic(err.Error())
 	}
+
+}
+
+// FilePathWalkDir function
+func FilePathWalkDir(root string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	return files, err
 }
